fca21ba62c94624deaa4238d50a68f10574581a5
thesagarmahajan <sagarmahajan31997@gmail.com>
2026-03-18 15:14:38 +0530

    apiIp Updated

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const apiIp = "localhost"
-export const apiPort = 8888
+export const apiIp = "13.205.18.198"
+export const apiPort = 80
 export const baseUrl = `http://${apiIp}:${apiPort}`
 export const postsUrl = `${baseUrl}/posts`
 export const articleUrl = (pid)=>`${postsUrl}/${pid}`
